670c0ec167b4fde16eae5b2d7fd7feb7a5b0d6a3
YTH4P0 <benedek.polgar@stud.uni-corvinus.hu>
2023-11-21 09:18:46 +0100

    elso

---
+++ WebAPIBase/wwwroot/scripts/pascalJS.js
@@ -0,0 +1,28 @@
+﻿window.onload = function () {
+	var drawPascal = function (sorszam) {
+		var pascaldiv = document.getElementById('pascal')
+		for (var sor = 0; sor < sorszam; sor++) {
+			var ujsor = document.createElement('div')
+			ujsor.classList.add('sor')
+			pascaldiv.appendChild(ujsor)
+
+			for (var oszlop = 0; oszlop <= sor; oszlop++) {
+				var ujelem = document.createElement('div')
+				ujelem.classList.add('elem')
+				ujelem.innerHTML = fakt(sor) / (fakt(oszlop) * fakt(sor - oszlop))
+				ujsor.appendChild(ujelem)
+			}
+		}
+	}
+}
+
+
+
+var fakt = function (a) {
+	if (a==1) {
+		return 1
+	}
+	else {
+		return a * fakt(a - 1)
+	}
+}
