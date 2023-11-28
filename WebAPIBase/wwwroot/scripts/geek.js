8a24e23bc962058a5e328e33c7bea926d1f05265
YTH4P0 <benedek.polgar@stud.uni-corvinus.hu>
2023-11-28 09:28:27 +0100

    11.ora

---
+++ WebAPIBase/wwwroot/scripts/geek.js
@@ -0,0 +1,30 @@
+﻿var viccek;
+var vicc;
+
+
+window.onload = function () {
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+}
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d);
+    viccek = d;
+
+    var main = document.getElementById('main');
+    for (var i = 0; i < viccek.length; i++) {
+        var ujvicc = document.createElement('div');
+        vicc = viccek[i].text;
+        for (var j = 0; j < vicc.length - 1; j++) {
+            if ((vicc[j] == 'a' || vicc[j] == 'q') && vicc[j + 1] == '.') {
+                vicc[j] = "\t";
+			}
+        }
+        ujvicc.innerHTML = `<p>${vicc}</p> <br>`;
+        main.appendChild(ujvicc);
+	}
+}
+
